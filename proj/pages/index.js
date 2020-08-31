6d0874c8a5940474f8f3c05b185b08326af70eab
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-08-31 02:30:09 -0500

    Commented out gray-matter

--- proj/pages/index.js
+++ proj/pages/index.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+// import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData
+//     }
+//   }
+// }
 
 export default function Home({ allPostsData }) {
   return (
