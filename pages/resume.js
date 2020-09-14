9ad4cddf04aa396385d26bc7582367349074f8dd
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-14 03:14:46 -0500

    Resume is Live!

--- pages/resume.js
+++ pages/resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NextLink from 'next/link';
 import Sidebar from '../components/Sidebar';
 import { Document, Page } from 'react-pdf';
-// import resume from './ResumeFall2020.pdf'
+//import resume from '../public/ResumeFall2020.pdf'
 
 import {
   Image,
@@ -18,17 +18,16 @@ import {
 } from '@chakra-ui/core';
 
 const Resume = () => (
-  <div id="Website">
+  <div id="Website" h="500%">
     <title>Andrew's Website</title>
     <Sidebar />
-      <Box bg="blue.100" h="100vh" w="85vw" right="0" position="fixed" display="inline-block" className="Background" padding={["20px", "20px", "100px", "200px"]}>
-        <Text fontSize={128} fontFamily="Lexend Deca">
-            Resume
-        </Text>
-        {/* <Document file={resume}>
-          <Page pageNumber={1} />
-        </Document> */}
-      </Box>
+    <Box bg="blue.100" h="500%" w="85vw" left="15vw" position="relative" display="inline-block" className="Background" px={["10px", "50px", "100px","200px"]}>
+      <Text fontSize={[64, 64, 100, 128]} fontFamily="Lexend Deca" ml={["20px", "20px", "50px", "100px"]}>
+          Resume
+      </Text>
+      <Image src="/images/ResumeFall2020-1.png" my={["10px", "20px", "50px", "50px"]}/>
+      <Image src="/images/ResumeFall2020-2.png"/>
+    </Box>
   </div>
 );
 
