dd3e4887a17b1f580a40da46f50456add5232774
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-15 12:55:18 -0500

    Filled in Bio

--- pages/favorites.js
+++ pages/favorites.js
@@ -21,8 +21,13 @@ const Favorites = () => (
     <title>Andrew's Website</title>
     <Sidebar />
       <Box bg="blue.100" h="100vh" w="85vw" right="0" position="fixed" display="inline-block" className="Background" padding={["10px", "20px", "100px", "200px"]}>
-        <Text fontSize={128} fontFamily="Lexend Deca">
+        <Text fontSize={[64, 64, 100, 128]} fontFamily="Lexend Deca">
           Favorites
+        </Text>
+
+        <Text fontSize={[32,32,50,64]} fontFamily="Lexend Deca" color="gray.500">
+          <br />
+          Coming Soon!
         </Text>
       </Box>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,16 @@ const Index = () => (
   <div id="Website">
     <title>Andrew's Website</title>
     <Sidebar />
-      <Box bg="blue.100" h="100vh" w="85vw" right="0" position="fixed" display="inline-block" className="Background" padding={["50px", "50px", "100px", "200px"]}>
-        <Text fontSize={[100, 100, 128, 128]} fontFamily="Lexend Deca">
-          Home
+      <Box bg="blue.100" h="100vh" w="85vw" right="0" position="fixed" display="inline-block" className="Background" 
+      px={["20px", "20px", "40px", "80px"]} py="6%">
+        <Text as="b" fontSize={[30, 30, 60, 80]} fontFamily="Lexend Deca" mb="30px">
+        Hello, I'm Andrew Deick!
+        </Text>
+        <Text fontSize={[25, 25, 40, 40]} fontFamily="Lexend Deca">
+          <br /> 
+          I am a Software Engineer at Iowa State, looking for a challenging
+          internship where I can learn new skills. Currently, my interests are in Virtual Reality, 
+          Front-End Development, and Machine Learning.
         </Text>
       </Box>
   </div>
