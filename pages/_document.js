e9c42065fb4992ea954845e27b1eb2e9b2a47341
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-01-06 13:49:43 -0600

    Upgraded NextJS view pages preview

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
             rel="stylesheet"
           />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" 
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
