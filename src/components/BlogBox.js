7d5038235b2298d12d5820864b752c3dd0b8a273
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-03-08 16:00:36 -0600

    Created Blog Page

---
+++ src/components/BlogBox.js
@@ -0,0 +1,64 @@
+import React, { Component, useState, useEffect } from 'react';
+import { Image, Center, VStack, Button, Box, Square, Circle , Collapse, Flex, Text, Tooltip, useDisclosure, useColorModeValue} from "@chakra-ui/react";
+import Link from 'next/link';
+
+// const useFavoritesBox = (props) => {
+//     let test = "test1";
+//     if(props.hash === window.location.hash){
+//         onOpen;
+//         test = "opened"
+//     }
+//     return { isOpen, onToggle, test };
+// }
+
+const BlogBox = (props) => {
+    // const {isOpen, onToggle, test } = useFavoritesBox(props);
+    const { isOpen, onToggle } = useDisclosure();
+    const [isHovering, setHovering] = useState(false);
+
+
+
+    // useEffect(() => {
+
+    // }, isOpen);
+    let content;
+    if(props.children){
+        if(props.hover){
+            if (isHovering){
+                content = props.hover;
+            }
+            else{
+                content = props.children;
+            }
+        }
+        else{
+            content = props.children;
+        }
+    }
+    else{
+        content = <Text fontSize={50} pt="30px" h="100%" w="100%" textAlign="center">🚧</Text>;
+    }
+
+
+    return (
+        <Link passHref href={props.href ? props.href : '/repo'}><a>
+        <Flex h="350px" w="350px" bg="gray.900" m="50px" border="15px solid black" borderRadius="75px" position="relative" overflow="hidden"
+        justifyContent="center"
+        onMouseOver={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        bg={isHovering ? useColorModeValue("blue.100","gray.800") : useColorModeValue("blue.200","gray.900")}
+        >
+            {content}
+            <Flex position="absolute" bottom="0" w="100%" h="100px" alignItems="center" justifyContent="center"
+            bg={isHovering ? useColorModeValue("blue.200","red.800") : useColorModeValue("blue.300","red.900")}
+            >
+            <Text fontFamily="Russo One" color="black" textAlign="center" fontSize={props.textSize ? props.textSize : 28}>
+                {props.text ? props.text : "Coming Soon"}
+            </Text>
+            </Flex>
+        </Flex>
+        </a></Link>
+    )
+}
+
+export default BlogBox;
