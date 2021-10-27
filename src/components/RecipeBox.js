fc94bf2cdd57143bea9204b14e11cda30dbfe202
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-10-27 16:12:54 -0500

    Fixed Bug on Recipe page
    Recipe Box would smush together on small pages

--- src/components/RecipeBox.js
+++ src/components/RecipeBox.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component, useState, useEffect } from 'react';
 import { Image, Center, VStack, Button, Box, Square, Circle, Collapse, Flex, Text, Tooltip, useDisclosure, useColorModeValue } from "@chakra-ui/react";
 import Link from 'next/link';
@@ -16,42 +17,41 @@ const RecipeBox = (props) => {
     const { isOpen, onToggle } = useDisclosure();
     const [isHovering, setHovering] = useState(false);
 
-
-
     // useEffect(() => {
-
     // }, isOpen);
 
     return (
         <Link passHref href={props.href ? props.href : '/recipes'}>
-            <Flex
-                h="250px"
-                w="350px"
-                m="50px"
-                border="15px solid black"
-                borderRadius="75px"
-                position="relative"
-                overflow="hidden"
-                onMouseOver={() => setHovering(true)}
-                onMouseLeave={() => setHovering(false)}
-                bg={isHovering ? useColorModeValue("blue.100", "gray.800") : useColorModeValue("blue.200", "gray.900")}
-            >
-                {props.children ? props.children :
-                    <Text fontSize={50} pt="30px" h="100%" w="100%" textAlign="center">🚧</Text>}
+            <a>
                 <Flex
-                    position="absolute"
-                    bottom="0"
-                    w="100%"
-                    h="50px"
-                    alignItems="center"
-                    justifyContent="center"
-                    bg={isHovering ? useColorModeValue("blue.200", "red.800") : useColorModeValue("blue.300", "red.900")}
+                    h="250px"
+                    w="350px"
+                    m="50px"
+                    border="15px solid black"
+                    borderRadius="75px"
+                    position="relative"
+                    overflow="hidden"
+                    onMouseOver={() => setHovering(true)}
+                    onMouseLeave={() => setHovering(false)}
+                    bg={isHovering ? useColorModeValue("blue.100", "gray.800") : useColorModeValue("blue.200", "gray.900")}
                 >
-                    <Text fontFamily="Russo One" color="black" fontSize={props.textSize ? props.textSize : 28}>
-                        {props.text ? props.text : "Coming Soon"}
-                    </Text>
+                    {props.children ? props.children :
+                        <Text fontSize={50} pt="30px" h="100%" w="100%" textAlign="center">🚧</Text>}
+                    <Flex
+                        position="absolute"
+                        bottom="0"
+                        w="100%"
+                        h="50px"
+                        alignItems="center"
+                        justifyContent="center"
+                        bg={isHovering ? useColorModeValue("blue.200", "red.800") : useColorModeValue("blue.300", "red.900")}
+                    >
+                        <Text fontFamily="Russo One" color="black" fontSize={props.textSize ? props.textSize : 28}>
+                            {props.text ? props.text : "Coming Soon"}
+                        </Text>
+                    </Flex>
                 </Flex>
-            </Flex>
+            </a>
         </Link>
     )
 }
